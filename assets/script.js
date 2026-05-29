26c353b08841a84a18a10fd423d38f0f5911d605
MBrunoLucky <marianocuestainmobiliaria@gmail.com>
2026-05-29 20:01:08 +0200

    Delete assets directory

--- assets/script.js
+++
@@ -1,3 +0,0 @@
-
-function acceptCookies(){localStorage.setItem('iage_cookie_ok','1');document.getElementById('cookieBox')?.setAttribute('hidden','hidden')}
-window.addEventListener('DOMContentLoaded',()=>{if(!localStorage.getItem('iage_cookie_ok'))document.getElementById('cookieBox')?.removeAttribute('hidden')});
